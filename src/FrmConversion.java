a271166e6c5e0ee0f4232f1096f463662e9e36c4
salas <salas@salas.local>
2025-07-29 14:09:20 -0500

    convertir a romano

--- src/FrmConversion.java
+++ src/FrmConversion.java
@@ -1,30 +1,68 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-public class FrmConversion extends JFrame{
+public class FrmConversion extends JFrame {
 
-    public  FrmConversion(){
-    setSize(300,200);                 //tamaño de la ventana
-    setTitle("Conversion a Romanos");   //asignar titulo
-    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);   // salir del programa
-    
-    setLayout(null);    // anular diseño
+    private JTextField txtNumero, txtRomano;
 
-    JLabel lblNumero= new JLabel("Número arábigo"); // creando etiqueta
-    lblNumero.setBounds(10, 10, 100, 25);  // ubicacion y dimensiones de la etiqueta
-    this.getContentPane().add(lblNumero);
-    
+    public FrmConversion() {
+        setSize(300, 200); // tamaño de la ventana
+        setTitle("Conversión a Romanos"); // asignar un titulo
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // salir del programa al cerrar
 
+        setLayout(null); // anular diseño
 
+        JLabel lblNumero = new JLabel("Número arábigo"); // creando una etiqueta
+        lblNumero.setBounds(10, 10, 100, 25); // ubicacion y dimensiones de la etiqueta
+        this.getContentPane().add(lblNumero);
 
+        txtNumero = new JTextField(); // creando una caja de texto
+        txtNumero.setBounds(110, 10, 100, 25); // ubicacion y dimensiones de la etiqueta
+        this.getContentPane().add(txtNumero);
 
+        txtRomano = new JTextField(); // creando una caja de texto
+        txtRomano.setBounds(110, 40, 100, 25); // ubicacion y dimensiones de la etiqueta
+        txtRomano.setEnabled(false);
+        this.getContentPane().add(txtRomano);
 
+        JButton btnCalcular = new JButton("A Romano");
+        btnCalcular.setBounds(10, 40, 100, 25);
+        getContentPane().add(btnCalcular);
 
+        btnCalcular.addActionListener(new ActionListener() {
 
-}
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                convertirARomano();
+            }
 
+        });
+    }
 
+    private void convertirARomano() {
+        int numero = Integer.parseInt(txtNumero.getText()); // convertir de texto a entero
 
+        String[] romanos = new String[] { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] arabigos = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+        String romano = "";
+        while (numero > 0) {
+            for (int i = 0; i < arabigos.length; i++) {
+                if (numero >= arabigos[i]) {
+                    romano = romano + romanos[i];
+                    numero = numero - arabigos[i];
+                    break;
+                }
+            }
+        }
+
+        txtRomano.setText(romano);
+    }
 
 }
